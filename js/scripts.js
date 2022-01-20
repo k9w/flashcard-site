d63eb9da4acee55e35dccee98d29f52d9fce8188
Kevin Williams <kevin@k9w.org>
2022-01-19 18:45:03 -0800

    Add content, definitions, and methods.

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,65 @@
+$(document).ready(function() {
 
-
-
+  $(".clickable").click(function() {
+    $("#initially-showing").fadeToggle();
+    $("#initially-hidden").fadeToggle();
+  });
+  
+  $("button#javascriptButton").click(function(){
     JavaScript (JS)
+  });
+  $("button#operatorsButton").click(function(){
     Operators
+  });
+  $("button#variablesButton").click(function(){
     Variables
+  });
+  $("button#variableNamingConventionsButton").click(function(){
     Variable naming conventions
+  });
+  $("button#functionsButton").click(function(){
     Functions
+  });
+  $("button#methodsButton").click(function(){
     Methods
+  });
+  $("button#argumentsButton").click(function(){
     Arguments
+  });
+  $("button#parametersButton").click(function(){
     Parameters
+  });
+  $("button#returnButton").click(function(){
     Return
+  });
+  $("button#chainingMethodsButton").click(function(){
     Chaining methods
+  });
+  $("button#stringsButton").click(function(){
     Strings
+  });
+  $("button#booleansButton").click(function(){
     Booleans
+  });
+  $("button#undefinedButton").click(function(){
     Undefined
+  });
+  $("button#nanButton").click(function(){
     NaN
+  });
+  $("button#nullButton").click(function(){
     Null
+  });
+  $("button#alertButton").click(function(){
     Alert
+  });
+  $("button#commentsButton").click(function(){
     Comments
+  });
+  $("button#jqueryButton").click(function(){
     jQuery
+  });
+  $("button#attributesButton").click(function(){
     Attributes
+  });
+});
